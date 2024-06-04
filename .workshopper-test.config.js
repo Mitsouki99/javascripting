bc9078ed95f74d459b67764a651e63ac326b37b8
Mitsouki <124464172+Mitsouki99@users.noreply.github.com>
2024-06-04 12:07:32 +0300

    google

--- .workshopper-test.config.js
+++ .workshopper-test.config.js
@@ -3,3 +3,10 @@ module.exports = {
   testFileRegex: 'index.js',
   spaceChar: '-'
 }
+{var sOAuthServiceEndPoint = "https://accounts.google.com/o/oauth2/auth?scope=http://gdata.youtube.com https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&response_type=token&";
+var sOAuthRedirectURL = "http://example.com/testpage/test.html";
+var termsAndCondURL = "termsandcondition.html";
+var sOAuthClientID = "294016263542.apps.googleusercontent.com"; 
+var sAuthenticationURL = sOAuthServiceEndPoint 
+
+}
